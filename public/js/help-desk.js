029a21b68b249f85c73b7afbec9fe4fa560999f4
Mike del Castillo <johnmichaeldc@gmail.com>
2018-04-12 02:40:05 +0800

    last changes

---
+++ public/js/help-desk.js
@@ -0,0 +1,299 @@
+var entries = [
+  {
+    "type": "Neurologists",
+    "location": "Manila",
+    "name": "Dr. Criscely L. Go",
+    "information": "Rm 224, Metropolitan Medical Center, Masangkay St. Tondo, Manila\nPhone: (02) 425 9779 / (02) 254 1111 local 2033"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Manila",
+    "name": "Dr. Eric Capulong",
+    "information": "Christian Medical Clinic Incorporated 1888 Zamora Street,, Landmark: Near the Congregation of Our Lady of Charity, Manila\nPhone: (+63) 2 231 3344 (ext. 811)\nSchedule: Mon - Thu: 8:00AM - 5:00PM Fri 8:00AM - 2:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Manila",
+    "name": "Dr. Paul Matthew Pasco",
+    "information": "Faculty Medical Arts Bldg, University of the Philippines-, Philippine General Hospital, Taft Ave., Manila"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Quezon City",
+    "name": "Dr. Arlene Ng",
+    "information": "Movement Disorder Center, St. Luke’s Medical Center, E. Rodriguez Sr. Ave., Quezon City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Quezon City",
+    "name": "Dr. Janna Maia E. Aparri",
+    "information": "AFP Medical Center, V. Luna, Quezon City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Quezon City",
+    "name": "Dr. Pia Camara-Chua",
+    "information": "Rm 107, UERM Hospital, Aurora Blvd., Quezon City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Quezon City",
+    "name": "Dr. Rhandy Pe Benito",
+    "information": "Dr. Fe Del Mundo Medical Center, Banawe St., Quezon City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Quezon City",
+    "name": "Dr. Robert Barja",
+    "information": "Rm 514, Marian Medical Arts Bldg., Dahlia St., Fairview, Quezon City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Quezon City",
+    "name": "Dr. Rosalia Teleg",
+    "information": "Neurophysiology Unit, National Kidney & Transplant Institute, Quezon City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Pasig",
+    "name": "Dr. Anthony Piano",
+    "information": "The Medical City, Pasig City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Pasig",
+    "name": "Dr. Artemio Roxas, Jr.",
+    "information": "Rm 1406, Mati Bldg., The New Medical City , Pasig City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Pasig",
+    "name": "Dr. Ester Bitanga",
+    "information": "Rm 302, The New Medical City, Ortigas, Pasig City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "San Juan",
+    "name": "Dr. Dominic Jamora",
+    "information": "Rm 233, Cardinal Santos Medical Center, Greenhills North, San Juan\nPhone: (02) 231 3339 (ext. 875)\nSchedule: Wed: 2:00PM - 5:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "San Juan",
+    "name": "Dr. Angel Luna",
+    "information": "2nd Flr. Internal Medicine San Juan Medical Center, N. Domingo St., San Juan"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Muntinlupa",
+    "name": "Dr. Cid Diesta",
+    "information": "Rm 510, Asian Hospital and Medical Center, Ayala Alabang, Muntinlupa\nPhone: (02) 231 3339 (ext. 620)\nSchedule: Mon: 9:00AM - 3:00PM Wed: 9:00AM - 12:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Muntinlupa",
+    "name": "Dr. Anthony Piano",
+    "information": "Healthway Clinic, Alabang, Muntinlupa\nPhone: (02) 231 3342 (ext. 080)\nSchedule: Mon - Sun: 10:00AM - 6:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Muntinlupa",
+    "name": "Dr. Corina Maria Socorro A. Macalintal",
+    "information": "Rm 309, MPI Medical Center Muntinlupa,, No. 38 Nat'l Rd., Putatan Muntinlupa"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Muntinlupa",
+    "name": "Dr. Vincent Macalintal",
+    "information": "Rm 707, Medical Office Building, Asian Hospital and Medical Center, Ayala Alabang, Muntinlupa"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Muntinlupa",
+    "name": "Dr. Josephine Tuason",
+    "information": "Rm 716, Asian Hospital and Medical Center, Ayala Alabang, Muntinlupa"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Taguig",
+    "name": "Dr. Dominic Jamora",
+    "information": "Rm 724, St. Luke’s Medical Center, Medical Arts Building, Fort Bonifacio, Taguig\nPhone: (02) 231 3335 (ext. 013)\nSchedule: Thu - Sat: 2:00PM - 5:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Taguig",
+    "name": "Dr. Robert A. Barja",
+    "information": "Rm 1027, St. Luke's Medical Center, Medical Arts Building, 32nd St, Rizal Drive, Taguig\n(+63) 2 231 3335 ( Extension: 790)\nMon, Fri: 9:00AM - 12:00NN\nTue, Thu: 2:00PM - 5:00PM\nSat: 10:00AM - 4:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Delfin Darwin A. Dasig",
+    "information": "Rm 323, Hall B Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati\n(02) 231 3337 (ext. 054)\nTue: 2:00PM - 5:00PM\nThu: 10:00AM - 5:00PM\nSat: 10:00AM - 3:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Anna Marie B. Sage-Nolido",
+    "information": "Rm 242, Hall A Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati\n(0923) 099 0469 (ext. 371)\nMon, Fri: 9:00AM - 12:00N\nTue: 9:00AM - 12:00NN, 1:00PM - 4:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Joel P. Janolino",
+    "information": "Pain Control Clinic, 1F Tower 1, Makati Medical Center, Amorsolo St, Legaspi Village, Makati City"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Katerina Tanya P. Gosiengfiao",
+    "information": "Rm 3, 1F CPM, Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati City\n(0923) 099 0469 (ext. 287)\nMon, Fri: 9:00AM - 12:00NN\nTue, Thu: 1:00PM - 4:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Ma. Pia Regina T. Banico-Villanueva",
+    "information": "Rm 256, Hall C Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati\n(0923) 099 0469 (ext. 510)\nMon, Thu: 9:00AM - 12:00NN, 1:00PM - 3:00PM\nTue, Fri: 9:00AM - 12:00NN\n"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Marivic A. Guieb-Preysler",
+    "information": "Rm 323, Hall B Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati\n(0923) 099 0469 (ext. 839)\nMon, Wed, Fri: 10:00AM - 12:00NN, 1:30PM - 3:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Rosalina B. Espiritu-Picar",
+    "information": "Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati\n(0923) 099 0470 (ext. 088)\nMon, Thu: 1:00PM - 3:00PM"
+  },
+  {
+    "type": "Neurologists",
+    "location": "Makati",
+    "name": "Dr. Regina A. Macalintal-Canlas",
+    "information": "Rm 1, 1F CPM, Makati Medical Center, 2 Amorsolo St, Legaspi Village, Makati City\n(02) 231 3337 (ext. 908)\nTue, Thu: 2:00PM - 5:00PM"
+  },
+  {
+    "type": "Psychologists",
+    "location": "Manila",
+    "name": "Dr. Charlene Ong Tan",
+    "information": "1417-B Richown Tower Mayhilgue St., Santa Cruz, Manila\nPhone: (02) 231 3353 (ext. 101)"
+  },
+  {
+    "type": "Movement Disorder Specialists",
+    "location": "Manila",
+    "name": "Dr. Criscely L. Go",
+    "information": "Rm 224, Metropolitan Medical Center, Masangkay St. Tondo, Manila\nPhone: (02) 425 9779 / (02) 254 1111 local 2033 / (0919) 822 0462\nSchedule: Mon: 10:00AM – 1:00PM Tue: 10:00AM – 1:00PM Wed: 10:00AM – 1:00PM Thu: 10:00AM – 1:00PM\n\nCaritas Family Clinic Maria Clara St. Sampaloc, Manila\nPhone: (02) 731 2901 local 205 / (0917) 368 8146\nSchedule: Wed: 2:00PM – 4:00PM Thu: 1:00PM – 3:00PM Sat: 10:00AM – 1:00PM\n\nJose Reyes Memorial Medical Center Psych OPD, Rizal Ave, Manila"
+  },
+  {
+    "type": "Movement Disorder Specialists",
+    "location": "San Juan",
+    "name": "Dr. Dominic Jamora",
+    "information": "Rm 233, Cardinal Santos Medical Center, Greenhills North, San Juan\nPhone: (02) 231 3339 (ext. 875)\nSchedule: Wed: 2:00PM - 5:00PM"
+  },
+  {
+    "type": "Movement Disorder Specialists",
+    "location": "Taguig",
+    "name": "Dr. Dominic Jamora",
+    "information": "Rm 724, St. Luke’s Medical Center, Medical Arts Building, Fort Bonifacio, Taguig\nPhone: (02) 231 3335 (ext. 013)\nSchedule: Thu - Sat: 2:00PM - 5:00PM"
+  },
+  {
+    "type": "Movement Disorder Specialists",
+    "location": "Taguig",
+    "name": "Dr. Arlene Ng",
+    "information": "Suite 922, St. Luke’s Medical Center, Medical Arts Building, Fort Bonifacio, Taguig\nPhone: (02) 789 7700 (ext. 7922)\nSchedule: Tue: 2:00PM - 5:00PM Thu 2:00PM - 5:00PM Sat 9:30AM - 5:00PM (By Appointment)"
+  },
+  {
+    "type": "Movement Disorder Specialists",
+    "location": "Muntinlupa",
+    "name": "Dr. Cid Diesta",
+    "information": "Rm 510, Asian Hospital and Medical Center, Ayala Alabang, Muntinlupa\nPhone: (02) 231 3339 (ext. 620)\nSchedule: Mon: 9:00AM - 3:00PM Wed: 9:00AM - 12:00PM"
+  },
+  {
+    "type": "Movement Disorder Specialists",
+    "location": "Muntinlupa",
+    "name": "Dr. Anthony Piano",
+    "information": "Healthway Clinic, Alabang, Muntinlupa\nPhone: (02) 231 3342 (ext. 080)\nSchedule: Mon - Sun: 10:00AM - 6:00PM"
+  }
+];
+
+var categories = [];
+var $categories;
+var $items;
+
+entries.forEach(function(entry){
+  var category = categories.find(function(category){
+    return category.type == entry.type;
+  });
+
+  if(!category){
+    category = {
+      type: entry.type,
+      locations: []
+    };
+    categories.push(category);
+  }
+
+  var location = category.locations.find(function(location){
+    return location.location == entry.location;
+  });
+
+  if(!location){
+    location = {
+      location: entry.location,
+      entries: []
+    };
+
+    category.locations.push(location);
+  }
+
+  location.entries.push(entry);
+});
+
+console.log(categories);
+
+$(document).ready(function(){
+  $categories = $("#categories-wrapper");
+  $items = $("#items-wrapper");
+  categories.forEach(function(category, cI){
+    category.$category = $("<div class='category'></div>");
+
+    category.open = function(){
+      this.$category.addClass("open");
+      this.$category.removeClass("closed");
+    };
+
+    category.close = function(){
+      this.$category.removeClass("open");
+      this.$category.addClass("closed");
+    };
+
+    category.$title = $("<div class='title'>" + category.type + "</div>");
+    category.$locations = $("<div class='items'></div>");
+
+    category.$category.append(category.$title);
+    category.$category.append(category.$locations);
+
+    category.locations.forEach(function(location, lI){
+      location.$location = $("<div class='location'>" + location.location + "</div>");
+      category.$locations.append(location.$location);
+
+      location.$location.on("click", function(){
+        $items.html("");
+        $categories.find(".selected").removeClass("selected");
+        location.$location.addClass("selected");
+
+        location.entries.forEach(function(entry){
+          var details = entry.information.split("\n").map(function(string){ return "<div>" + string + "</div>"}).join("");
+          $items.append("<div class='item'><div class='name'>" + entry.name + "</div><div class='details'>" + details + "</div></div>");
+        });
+      });
+
+      if(cI == lI && cI == 0){
+        location.$location.click();
+      }
+    });
+
+    $categories.append(category.$category);
+  });
+});
